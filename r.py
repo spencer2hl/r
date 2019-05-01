821b4d3482c3fa3bd721b846cf30197fa0f4d412
spencer2hl <frost0901@outlook.com>
2019-05-01 14:39:03 -0500

    second version

--- r.py
+++ r.py
@@ -1,7 +1,9 @@
 import random
 
 r = random.randint(1, 100) # 包含了1和100
+count = 0
 while True:
+	count = count + 1 #count += 1 快写法
 	num = input('请猜数字：')
 	num = int(num)
 	if num == r:
@@ -11,3 +13,4 @@ while True:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('这是你猜的第', count, '次')
